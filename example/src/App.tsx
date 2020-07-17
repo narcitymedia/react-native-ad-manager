740b08c7055f67b031f915a7f361fa29ecba3e15
emeraldsanto <yanick.belanger@yahoo.com>
2020-07-17 12:34:34 -0400

    Added ad manager pod

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,25 +1,25 @@
 import * as React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import AdManager from 'react-native-ad-manager';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
+	const [result, setResult] = React.useState<number | undefined>();
 
-  React.useEffect(() => {
-    AdManager.multiply(3, 7).then(setResult);
-  }, []);
+	React.useEffect(() => {
+		AdManager.multiply(3, 7).then(setResult);
+	}, []);
 
-  return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Text>Result: {result}</Text>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
--- react-native-ad-manager.podspec
+++ react-native-ad-manager.podspec
@@ -18,4 +18,5 @@ Pod::Spec.new do |s|
   
 
   s.dependency "React"
+  s.dependency "Google-Mobile-Ads-SDK"
 end
