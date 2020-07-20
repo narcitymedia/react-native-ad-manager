d065385d3f14694163dd5b8a1de4677effcfa088
emeraldsanto <yanick.belanger@yahoo.com>
2020-07-20 13:34:01 -0400

    Private methods + event dispatcher

--- android/src/main/java/com/reactnativeadmanager/BannerView.kt
+++ android/src/main/java/com/reactnativeadmanager/BannerView.kt
@@ -2,23 +2,23 @@ package com.reactnativeadmanager
 
 import android.content.Context
 import android.util.Log
-import com.facebook.react.bridge.ReactContext
-import com.facebook.react.bridge.ReadableArray
-import com.facebook.react.bridge.ReadableMap
+import com.facebook.react.bridge.*
+import com.facebook.react.uimanager.events.RCTEventEmitter
 import com.facebook.react.views.view.ReactViewGroup
 import com.google.android.gms.ads.AdSize
 import com.google.android.gms.ads.MobileAds
 import com.google.android.gms.ads.RequestConfiguration
 import com.google.android.gms.ads.doubleclick.PublisherAdRequest
 import com.google.android.gms.ads.doubleclick.PublisherAdView
+import com.reactnativeadmanager.constants.AdEvent
 import java.lang.Exception
 import java.util.Arrays
 
 
 class BannerView(context: Context): ReactViewGroup(context) {
 
-	private var adView: PublisherAdView = PublisherAdView(context);
-	private var adRequestBuilder: PublisherAdRequest.Builder = PublisherAdRequest.Builder();
+	private var adView: PublisherAdView = PublisherAdView(context)
+	private var adRequestBuilder: PublisherAdRequest.Builder = PublisherAdRequest.Builder()
 
 	// region PRIVATE METHODS
 	private fun loadAd() {
@@ -26,21 +26,22 @@ class BannerView(context: Context): ReactViewGroup(context) {
 	}
 
 	private fun addAdView() {
-
+		this.addView(this.adView)
 	}
 
 	private fun destroyAdView() {
-
+		this.adView.destroy()
 	}
 
 	private fun removeAdView() {
-
+		this.removeView(this.adView)
 	}
 	// endregion
 
 	// region PUBLIC METHODS
 	fun loadBanner() {
-
+		this.sendJSEvent(AdEvent.REQUEST);
+		this.adView.loadAd(this.adRequestBuilder.build())
 	}
 
 	fun addBannerView() {
@@ -57,7 +58,7 @@ class BannerView(context: Context): ReactViewGroup(context) {
 
 	fun openDebugMenu() {
 		if (this.adView.adUnitId.isNullOrEmpty()) {
-			return;
+			return
 		}
 
 		MobileAds.openDebugMenu(
@@ -119,6 +120,24 @@ class BannerView(context: Context): ReactViewGroup(context) {
 	// endregion
 
 	// region UTILITY METHODS
+	private fun sendJSEvent(event: AdEvent, args: WritableMap? = Arguments.createMap()) {
+		try {
+			(context as ReactContext)
+				.getJSModule(RCTEventEmitter::class.java)
+				.receiveEvent(
+					this.id,
+					event.name,
+					args
+				)
+		}
 
+		catch (exception: Exception) {
+			Log.w(AdManagerModule.MODULE_NAME, "Unable to send event ${event.name} - ${exception.localizedMessage}")
+		}
+	}
+
+	private fun setAdListeners() {
+
+	}
 	// endregion
 }
